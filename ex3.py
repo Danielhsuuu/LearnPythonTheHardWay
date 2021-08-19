34534e62ca3cf9ee0cb0265d78d41367480393f1
Danielhsuuu <danielhsu0614@gmail.com>
2021-08-19 14:16:26 +0800

    Create ex3.py

---
+++ ex3.py
@@ -0,0 +1,13 @@
+print("1 + 1 =", 1 + 1)
+print("1 - 1 =", 1 - 1)
+
+print("1 * 1 =", 1 * 1)
+
+print("Is it true that 1 > 2?")
+
+print(1 > 2)
+
+print("What is 123 * 456?", 123*456)
+print("What is 123 / 456?", 123/456)
+
+print("What is 1000 % 123?", 1000%123)
