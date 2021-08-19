bb5ff40d49be93395054add9164cf2eb4a96e7fb
Danielhsuuu <danielhsu0614@gmail.com>
2021-08-19 14:34:26 +0800

    Create ex5.py

---
+++ ex5.py
@@ -0,0 +1,17 @@
+name = "Daniel Hsu"
+age = 17
+height = 181 # cm
+weight = 90 #kg
+eyes = "Black"
+teeth = "White"
+hair = "Black"
+
+print(f"Let's talk about {name}.")
+print(f"I'm {height} centimeters tall.")
+print(f"I'm {weight} kilograms heavy.")
+print("Actually that's quite heavy.")
+print(f"I've got {eyes} eyes and {hair} hair.")
+print(f"My teeth are usually {teeth} depending on the coffee.")
+
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
