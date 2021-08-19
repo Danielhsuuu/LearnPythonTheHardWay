850bbf125a8a9214903df3773d2ddd290d9b3784
Danielhsuuu <danielhsu0614@gmail.com>
2021-08-19 14:51:08 +0800

    Create ex7.py

---
+++ ex7.py
@@ -0,0 +1,39 @@
+#prints "Mary had a little lamb."
+print("Mary had a little lamb.")
+#prints "Its fleece was white as snow."
+print("Its fleece was white as {}.".format('snow'))
+#prints "And everywhere that Mary went."
+print("And everywhere that Mary went.")
+#prints ten "."
+print("." * 10)  # what'd that do?
+
+#declares a string named end1 containing "C"
+end1 = "C"
+#declares a string named end2 containing "h"
+end2 = "h"
+#declares a string named end3 containing "e"
+end3 = "e"
+#declares a string named end4 containing "e"
+end4 = "e"
+#declares a string named end5 containing "s"
+end5 = "s"
+#declares a string named end6 containing "e"
+end6 = "e"
+#declares a string named end7 containing "B"
+end7 = "B"
+#declares a string named end8 containing "u"
+end8 = "u"
+#declares a string named end9 containing "r"
+end9 = "r"
+#declares a string named end10 containing "g"
+end10 = "g"
+#declares a string named end11 containing "e"
+end11 = "e"
+#declares a string named end12 containing "r"
+end12 = "r"
+
+# watch end = ' ' at the end.  try removing it to see what happens
+#prints out "Cheese"
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+#prints out "Burger"
+print(end7 + end8 + end9 + end10 + end11 + end12)
