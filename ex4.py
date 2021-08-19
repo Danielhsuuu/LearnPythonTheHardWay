2c65e72af2b7d37ee8d5da85da378d10eefac0b4
Danielhsuuu <danielhsu0614@gmail.com>
2021-08-19 14:26:31 +0800

    Create ex4.py

---
+++ ex4.py
@@ -0,0 +1,9 @@
+peopleInPAS = 300
+Male = 140
+Female = peopleInPAS - Male
+Students = 270
+Teachers = peopleInPAS - Students
+
+print("There are a total of ", peopleInPAS," people in PAS")
+print("There are ", Male," males and ", Female, "females")
+print("There are ", Students, "students and ", Teachers, " teachers")
