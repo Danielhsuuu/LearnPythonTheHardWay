df6c858eb0a4abbb7cc31cffe190d26cf9a8e471
Danielhsuuu <danielhsu0614@gmail.com>
2021-08-19 14:08:38 +0800

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -1,1 +1,9 @@
-blahh
+#You can't see me
+#You can't see me
+#You can't see me
+#You can't see me
+#You can't see me
+#You can't see me
+#You can't see me
+print("Now you can see me")
+#You can't see me again
